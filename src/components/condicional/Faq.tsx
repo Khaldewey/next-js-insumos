663da1d3058ef9bcfcb2488d5cbac7dd9c8cf058
israel <Israel@AGENCIAACERTE.LOCAL>
2024-07-23 10:49:53 -0300

    componentes condicionais

--- src/components/condicional/Faq.tsx
+++ src/components/condicional/Faq.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Pergunta from "./Pergunta";
 
 export default function Faq() {
+  const [ativo, setAtivo] = useState<number>(0)
+
+  function alternarVisibilidade(indice: number) {
+    if (indice === ativo){
+        setAtivo(-1)
+    } else {
+        setAtivo(indice)
+    }
+  }
   return (
-    <div>
-        <Pergunta/>
-        <Pergunta/> 
-        <Pergunta/> 
-        <Pergunta/>
+    <div className="flex flex-col gap-2 w-[90%] md:w-3/4">
+        <Pergunta aberta={ativo === 0} indice={0} alternarVisibilidade={alternarVisibilidade} texto=" O que é React? " resposta="React é um framework JavaScript criado pelo Facebook (atual Meta) que é usado para criar interfaces de usuário (UI) em aplicativos web. "/>
+        <Pergunta aberta={ativo === 1} indice={1} alternarVisibilidade={alternarVisibilidade} texto="O que é Spring Boot?" resposta="Spring Boot é uma estrutura Java de código aberto usada para programar aplicativos autônomos e de nível de produção baseados em Spring com esforço mínimo."/> 
+     
     </div>
   )
 }
--- src/components/condicional/Pergunta.tsx
+++ src/components/condicional/Pergunta.tsx
@@ -1,16 +1,27 @@
 import { useState } from "react"
-
-export default function Pergunta() {
+import If from "./If"
+interface PerguntaProps {
+  indice: number
+  texto: string 
+  resposta: string
+  aberta: boolean
+  alternarVisibilidade: (indice: number) => void
+}
+export default function Pergunta(props: PerguntaProps) {
   
-  const [aberta, setAberta] = useState<boolean>(false)
+  // const [aberta, setAberta] = useState<boolean>(false)
 
   return (
-    <div className="border border-white rounded-md pd-5 overflow-hidden">
-       <div className="bg-zinc-700 p-5 cursor-pointer" onClick={ () => setAberta(!aberta)}> Pergunta </div>
+    <div className="border border-zinc-300 rounded-md pd-5 overflow-hidden">
+       <div className="bg-zinc-700 p-5 cursor-pointer select-none flex justify-between" onClick={ () => props.alternarVisibilidade(props.indice)}><span> { props.texto }</span>  </div>
        {/* {aberta ? "Sim" : "Não"} */}
-       {aberta && (
-        <div className="p-5"> Resposta </div>
-       )}
+       
+       <If teste={props.aberta}>
+        <div className="p-5"> {props.resposta} </div>
+       </If>
+       {/* {aberta && (
+        <div className="p-5"> {props.resposta} </div>
+       )} */}
        
     </div>
   )
