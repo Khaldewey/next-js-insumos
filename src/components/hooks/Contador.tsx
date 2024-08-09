2119f838aca4efab87e028f34966085d89ad8ca4
Israel Alves <israel_alves77@hotmail.com>
2024-08-09 16:28:52 -0300

    useEffect video 5 pare no 32:33

---
+++ src/components/hooks/Contador.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+export default function Contador() {
+  const [valor, setValor] = useState(0)
+  const [multi10, setMulti10] = useState(0)
+
+  function calcularValorMulti10(){
+    return valor * 10
+  }
+  
+  useEffect(() => {
+    const multi10 = calcularValorMulti10()
+    setMulti10(multi10)
+  }, [valor, calcularValorMulti10 ])
+  
+  
+  return (
+    <div className="flex flex-col items-center gap-5">
+        <span className="text-5xl">{valor}</span>
+        <span className="text-3xl text-zinc-500">{multi10}</span>
+        <div className="flex gap-5">
+            <button className="botao" onClick={ () => setValor( valor - 1)}>-1</button>
+            <button className="botao" onClick={ () => setValor(valorAtual => valorAtual + 1)}>+1</button>
+        </div>
+    </div>
+  )
+}
